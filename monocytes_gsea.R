dae76d4656eb5048c700c52385455223e119ee32
Schäpertöns Veronika <veronika.schaepertoens@plus.ac.at>
2023-03-24 13:36:09 +0100

    minor changes, reordering

--- monocytes_gsea.R
+++ monocytes_gsea.R
@@ -3,10 +3,9 @@
 
 # import libraries ---------------------------------------------------------
 library(readxl)
-library(data.table)
-library(fgsea)
 library(tidyverse)
 library(janitor)
+library(fgsea)
 
 # import data -------------------------------------------------------------
 
@@ -14,14 +13,14 @@ df_data <- read_xlsx("data/Monocytes_Experiment_Results_20230316.xlsx", sheet = 
   clean_names()
 
 
-# map to gene names -------------------------------------------------------
+# clean gene names -------------------------------------------------------
 
 df_data['gene'] <- gsub(" .+$", "", df_data$gene_names)
 
 
 # load genesets(actually pathways for fgsea) ------------------------------
 
-genesets <- fread("02_Enr_Genesets/Table.tsv")
+genesets <- read_tsv("database/enrichr/enrichr_database.tsv")
 
 
 # enrichment analysis using fgsea -----------------------------------------
@@ -32,8 +31,9 @@ gsea.res <- data.table(fgsea(
   stats = with(df_data, setNames(object = df_data$coef_t3alwofvs_t2hpyl, nm = df_data$gene)),
   nperm=1e6
   )
-  gsea.res$leadingEdge <- sapply(gsea.res$leadingEdge, function(vec) paste(vec, collapse = ","))
 )
+
+gsea.res$leadingEdge <- sapply(gsea.res$leadingEdge, function(vec) paste(vec, collapse = ","))
 
 dim(gsea.res[padj < 0.05])
 
@@ -41,5 +41,5 @@ gsea.res[padj < 0.05]
 
 gsea.res[padj < 0.05][,-"leadingEdge"][order(padj)]
 
-write_tsv(x = gsea.res, file = "gsea_alwof_vs_hpyl.tsv")
+write_tsv(x = gsea.res, file = "analysis/gsea_alwof_vs_hpyl.tsv")
 
