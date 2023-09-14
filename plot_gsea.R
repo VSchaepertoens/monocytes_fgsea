298aa87a18a52efe5ed61db38a8abc013e562809
VSchaepertoens <vschaepertoens@protonmail.com>
2023-09-14 16:55:51 +0200

    writes function for plotting

--- plot_gsea.R
+++ plot_gsea.R
@@ -64,60 +64,52 @@ gsea.res <-
   left_join(pathway_short_names, by = "pathway")
 
 
+# make plots --------------------------------------------------------------
 
-# bar graph ---------------------------------------------------------------
+plot_gsea <- function(grp, plot_title, revert = FALSE) {
+  data_to_plot <- 
+    gsea.res %>% 
+    filter(padj < 0.05, grp == {{grp}}) %>% 
+    arrange(padj)
+  
+  if (revert)
+    data_to_plot <- 
+      data_to_plot %>% 
+      mutate(NES = -NES)
+  
+  ggplot(data_to_plot) +
+    geom_col(
+      mapping = aes(x = NES,
+                    y = reorder(pathway_short, NES),
+                    fill = -log10(padj))
+    ) +
+    xlab("normalized enrichment score") +
+    ylab("gene sets") +
+    ggtitle(plot_title) +
+    scale_fill_gradient(
+      low = "black",
+      high = "red",
+      name = expression(-log[10](p[adj]))
+    ) +
+    theme_bw() +
+    theme(text = element_text(size = 10))
+}
 
-png("figures/gsea_hp_vs_aci_600dpi.png",
-    height = 150,
-    width = 150,
-    unit = "mm",
-    res = 600)
+# aci vs hp
+plot_gsea("alwof_vs_hpyl", "Enriched in Hp vs Aci", revert = TRUE)
 
-ggplot(data = data_to_plot) +
-  geom_col(
-    mapping = aes(x = (NES*-1),
-                  y = reorder(pathway, NES),
-                  fill = -log10(padj))
-  ) +
-  scale_fill_paletteer_c("ggthemes::Classic Red-Black", direction = -1) +
-  xlab("normalized enrichment score") +
-  ylab("gene sets") +
-  ggtitle("Enriched in Hp vs Aci") +
-  scale_fill_gradient(low = "black", high = "red", name = expression(-log[10](p[adj]))) +
-  theme_bw() +
-  theme(text = element_text(size = 10, family = "sans"))
-
-dev.off()
+ggsave("figures/gsea_hp_vs_aci_600dpi.png",
+       height = 150,
+       width = 300,
+       units = "mm",
+       dpi = 600)
 
 
-# lps vs hp ---------------------------------------------------------------
+# lps vs hp
+plot_gsea("hpyl_vs_lps", "Enriched in Hp vs LPS")
 
-data_to_plot <- gsea.res[padj < 0.05][,-"leadingEdge"][grp == "hpyl_vs_lps"][order(padj)]
-
-write.table(data_to_plot, "analysis/gsea_hpyl_lps.tsv")
-
-data_to_plot <- read_delim("analysis/gsea_hpyl_lps_short.tsv",
-                           delim = " ")
-# bar graph ---------------------------------------------------------------
-
-png("figures/gsea_hp_vs_lps_600dpi.png",
-    height = 200,
-    width = 200,
-    unit = "mm",
-    res = 600)
-
-ggplot(data = data_to_plot) +
-  geom_col(
-    mapping = aes(x = (NES),
-                  y = reorder(pathway, NES),
-                  fill = -log10(padj))
-  ) +
-  scale_fill_paletteer_c("ggthemes::Classic Red-Black", direction = -1) +
-  xlab("normalized enrichment score") +
-  ylab("gene sets") +
-  ggtitle("Enriched in Hp vs LPS") +
-  scale_fill_gradient(low = "black", high = "red", name = expression(-log[10](p[adj]))) +
-  theme_bw() +
-  theme(text = element_text(size = 10, family = "sans"))
-
-dev.off()
+ggsave("figures/gsea_hp_vs_lps_600dpi.png",
+       height = 200,
+       width = 300,
+       units = "mm",
+       dpi = 600)
